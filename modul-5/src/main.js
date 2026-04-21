6144de6e31dae9f2386ba440aa2f3e722a3e979d
FqrlRazi <140995442+FqrlRazi@users.noreply.github.com>
2026-04-21 10:49:28 +0700

    bagian-4-komponen UI & dark mode

--- modul-5/src/main.js
+++ modul-5/src/main.js
@@ -1,1 +1,15 @@
-import './style.css'
+import "./style.css";
+// Dark mode toggle
+const btn = document.getElementById("toggleDark");
+const html = document.documentElement;
+// Cek preferensi tersimpan
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  btn.textContent = "light";
+}
+btn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+  const isDark = html.classList.contains("dark");
+  btn.textContent = isDark ? "Light" : "Dark";
+  localStorage.setItem("theme", isDark ? "Dark" : "ight");
+});
